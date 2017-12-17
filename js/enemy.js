5c4520ba268bba5ecc36824b61436bdf77947252
iorehovski <iorehovski@gmail.com>
2017-12-17 18:55:43 +0300

    fix enemies movement

--- js/enemy.js
+++ js/enemy.js
@@ -31,7 +31,7 @@ class Enemy {
 
         let dx = playerPos.x - this.pos.x;
         let dy = playerPos.y - this.pos.y;
-        if((Math.abs(dx) <= 100)) {
+        if(this.pos.x <= 400 || playerPos.x <= 400 ||  (Math.abs(dx) <= 100)) {
             if(dx > 0) {    
                 this.pos.x += 1;
             } else if(dx < 0) {
@@ -55,7 +55,7 @@ class Enemy {
 
             let currentX = this.pos.x;
             for(let k = 0; k < 15; k++) {
-                if(Math.abs( playerPos.x - currentX ) < 100) { 
+                if(currentX < 400 || playerPos.x < 400 || Math.abs( playerPos.x - currentX ) < 100) { 
                     break;
                  }
                 resultY = arrY[0];
@@ -71,7 +71,6 @@ class Enemy {
                         if(s != 0) {
                             difference += arrY[j] / s;
                         }
-                   
                     }
                     for(let m = 0; m < i; m++) {
                         let findX = currentX;
