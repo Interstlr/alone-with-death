f5c7972763bb661d0a818bc4ee5ff82f548dccd1
iorehovski <iorehovski@gmail.com>
2017-12-17 18:40:23 +0300

    update movement of enemies

--- js/enemy.js
+++ js/enemy.js
@@ -2,37 +2,109 @@ class Enemy {
     constructor(x, y, r) {
         this.r = r;
         this.pos = createVector(x, y);
-        this.moveSpeed = 2;
+        this.moveSpeed = 7;
         this.color = color(255);
 
         this.hp = 100;
         this.damage = 0;
         this.damageToWall = 2;
 
+        this.moveQueue = [];
+
         this.isOnScreen = false;
     }
 
     update(playerPos, map) {
 
-        let dx = playerPos.x - this.pos.x;
-        let dy = playerPos.y - this.pos.y;
+       
 
         let moveX = 0;
         let moveY = 0;
 
-        if(dx > 0) {
-            this.pos.x += 1;
-            moveX += 1;
-        } else if(dx < 0) {
-            this.pos.x -= 1;
+        
+       
+        // if(dy >= 0) {
+        //     moveY = this.pos.y * 2;
+        // } else{
+        //     moveY = this.pos.y / 2;
+        // }
+
+        let dx = playerPos.x - this.pos.x;
+        let dy = playerPos.y - this.pos.y;
+        if((Math.abs(dx) <= 100)) {
+            if(dx > 0) {    
+                this.pos.x += 1;
+            } else if(dx < 0) {
+                this.pos.x -= 1;
+            }
+
+            if(dy > 0) {
+                this.pos.y += 1;
+            } else{
+                this.pos.y -= 1;
+            }
+        }
+        if(this.moveQueue.length == 0) {
+            
+            let arrX = [this.pos.x, (this.pos.x + playerPos.x)  / 1, playerPos.x];
+            let arrY = [this.pos.y,  (this.pos.y + playerPos.y) / 1, playerPos.y];
+
+            let nPoints = arrX.length;
+            let resultY = 0,
+                s = 0;
+
+            let currentX = this.pos.x;
+            for(let k = 0; k < 15; k++) {
+                if(Math.abs( playerPos.x - currentX ) < 100) { 
+                    break;
+                 }
+                resultY = arrY[0];
+                for(let i = 1; i < nPoints; i++) {
+                    let difference = 0;
+                    for(let j = 0; j <= i; j++) {
+                        s = 1;
+                        for(let m = 0; m <= i; m++) {
+                            if(m != j) {
+                                s *= arrX[j] - arrX[m];
+                            }
+                        }
+                        if(s != 0) {
+                            difference += arrY[j] / s;
+                        }
+                   
+                    }
+                    for(let m = 0; m < i; m++) {
+                        let findX = currentX;
+                        difference *= (findX - arrX[m]);
+                    }
+                    resultY += difference;
+                }
+
+                this.moveQueue.push(createVector(currentX,resultY));
+
+                if(dx > 0) {    
+                    currentX += 1;
+                } else if(dx <= 0) {
+                    currentX -= 1;
+                }
+            }
+            // console.log(this.moveQueue);
+        }else {
+            this.pos.x = this.moveQueue[0].x;
+            this.pos.y = this.moveQueue[0].y;
+            this.moveQueue.splice(0,1);
         }
 
-        if(dy > 0) {
-            this.pos.y += 1;
-        } else if(dy < 0) {
-            this.pos.y -= 1;
-        }
+      
+        // this.pos.y = resultY;
+        // console.log('x: ' + this.pos.x + ' y:' + resultY);
+        // if(dy >= 0) {
+        //     this.pos.y += 1;
+        // } else{
+        //     this.pos.y -= 1;
+        // }
 
+       
         this.checkCollidingWalls(map);
 
         if(this.isOnScreen) {
