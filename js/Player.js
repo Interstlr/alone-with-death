62d5ef7e80904e96cdd5104bc53802acb6bcca16
Intertlr <d.papchenko@gmail.com>
2017-12-17 22:02:01 +0300

    decreased player speed

--- js/Player.js
+++ js/Player.js
@@ -11,8 +11,8 @@ class Player {
 
 		this.queueBullets = null;
 
-		this.playerSpeed = 8;
-		this.boostedPlayerSpeed = this.playerSpeed * 4;
+		this.playerSpeed = 6;
+		this.boostedPlayerSpeed = this.playerSpeed * 1.5;
 
 		this.barsX = 10;
 		this.barsY = 200;
@@ -177,7 +177,7 @@ class Player {
 				this.blockRunning = true;
 			}
 		} else {
-			this.playerSpeed = this.boostedPlayerSpeed / 4;
+			this.playerSpeed = this.boostedPlayerSpeed / 1.5;
 		}
 	}
 
